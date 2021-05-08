3dedfadc5b60b1c2fc76a719802689bf96b247e6
kento <kento@toriwatari.bird>
2021-05-08 21:13:18 +0900

    Develop index.vue

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    CSS Animation & Tips
+
+    <a href="/01/">01</a>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style scoped>
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
