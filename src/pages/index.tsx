eb80f55d46d8d7b850531cf77f1e6fa300102a66
kento <kento@toriwatari.bird>
2021-10-24 20:07:56 +0900

    💪 #11 tsx化

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const BlogIndex = () => {
+  return (
+    <Layout>
+      <h1>CSS Animation & Tips</h1>
+    </Layout>
+  )
+}
+
+export default BlogIndex
