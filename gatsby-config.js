05eea0950c139e6387b5e6a64e66fa8528e50ac6
kento <kento@toriwatari.bird>
2021-10-25 23:32:51 +0900

    blog-post.jsを作成

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `src/pages`,
+        path: `src/posts`,
         name: `posts`
       }
     },
--- src/posts/index.tsx
+++ src/posts/index.tsx
@@ -1,12 +1,9 @@
 import React from "react"
 
-import Test from "./01/test"
-
 export default function Home() {
   return (
     <>
-    <div>Hello world!</div>
-    <Test />
+      <div>Hello world!</div>
     </>
   )
 }
