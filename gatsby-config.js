41e54caafa4de8afb1406822b46c4bdd84573404
kento <kento@toriwatari.bird>
2021-10-25 21:22:01 +0900

    💪 mdxのページ生成確認

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `src/pages`,
+        name: `posts`
+      }
+    },
+  ],
 }
