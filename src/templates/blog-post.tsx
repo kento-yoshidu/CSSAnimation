cf371c457c86acbf1c3dd2040218368f983a6c90
kento <kento@toriwatari.bird>
2021-10-26 21:25:31 +0900

    🏃‍♂️ update

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -2,17 +2,25 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import MDXRenderer from "gatsby-plugin-mdx/mdx-renderer"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 
+
+const Styles = require("../styles/_blogTemplate.module.scss")
+
+import 'prismjs/themes/prism-twilight.css'
 interface Props {
   data?: string
   location: string
 }
-const Blog = ({ data, location }) => (
+const Blog: React.VFC<Props> = ({ data, location }) => (
   <Layout
     location={location}
   >
-    <MDXRenderer frontmatter={data.mdx.frontmatter}>{data.mdx.body}</MDXRenderer>
+    <MDXRenderer
+      frontmatter={data?.mdx?.frontmatter}
+    >
+      {data?.mdx?.body}
+    </MDXRenderer>
   </Layout>
 )
 export default Blog
