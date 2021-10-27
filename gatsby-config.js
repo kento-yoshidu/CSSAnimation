8939d71985b03d35ecfdd0aaae16dc08ee7c97c0
kento <kento@toriwatari.bird>
2021-10-27 23:41:08 +0900

    ✨ #11 gatsby-configのts化

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,7 @@
-module.exports = {
-  plugins: [
-    `gatsby-plugin-typescript`,
-    //`gatsby-plugin-typegen`,
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 1200,
-              linkImagesToOriginal: false,
-            },
-          },
-          {
-          resolve: "gatsby-remark-prismjs",
-            options: {
-              classPrefix: "language-",
-              inlineCodeMarker: null,
-              showLineNumbers: false,
-            }
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `src/posts`,
-        name: `posts`
-      }
-    },
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-sass`,
-  ],
-}
+const { register } = require('esbuild-register/dist/node')
+
+register({
+  target: 'node16'
+})
+
+module.exports = require('./gatsby-config.ts')
