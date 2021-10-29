c2251882fae2ba85a5bd215e285aaed507940cb6
kento <kento@toriwatari.bird>
2021-10-29 13:49:50 +0900

    ✨ footerコンポーネントの作成

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Styles = require("../styles/_footer.module.scss")
+
+const Footer = () => (
+  <footer className={Styles.footer}>
+    <h1>CSS Animation & Tips</h1>
+  </footer>
+)
+
+export default Footer
