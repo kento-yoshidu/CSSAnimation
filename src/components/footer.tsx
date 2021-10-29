838bc8cc6294529175d358fe358a042316cda9f2
kento <kento@toriwatari.bird>
2021-10-29 13:59:54 +0900

    🎨 #38 CSS修正

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Styles = require("../styles/_footer.module.scss")
 
 const Footer = () => (
   <footer className={Styles.footer}>
     <h1>CSS Animation & Tips</h1>
+
+    <Link to="/">Home</Link>
   </footer>
 )
 
