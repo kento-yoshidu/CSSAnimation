98c5f946211207ee64bf8e1a3b226a6453bfc5bf
kento <kento@toriwatari.bird>
2021-10-30 20:03:58 +0900

    ✨ 02を作成

---
+++ src/components/02/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Styles = require("./styles.module.scss")
+
+const Component02 = () => (
+  <div className={Styles.box}>
+    <p>#02</p>
+    <p>underline</p>
+  </div>
+)
+
+export default Component02
